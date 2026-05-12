20d02143ba9c7e45892a7aad9f0818d6ea56eae9
Renef <renefricardo30@gmail.com>
2026-05-12 16:46:02 -0300

    implementa filtros de minimo (erosão), maximo ( dilatacao ) e midpoint( ponto medio)

--- filtros.py
+++ filtros.py
@@ -96,3 +96,171 @@ def filtro_gaussiana(img_bgr, tamanho_kernel=3):
     )
 
     return img_rgb
+
+def filtro_minimo(img_bgr, tamanho_kernel=3):
+
+    img_lab = cv2.cvtColor(
+        img_bgr,
+        cv2.COLOR_BGR2LAB
+    ).astype(np.float64)
+
+    l, a, b = cv2.split(img_lab)
+
+    h, w = l.shape
+
+    l_filtrado = np.zeros_like(l)
+
+    margem = tamanho_kernel // 2
+
+    l_pad = np.pad(
+        l,
+        ((margem, margem),
+         (margem, margem)),
+        mode='edge'
+    )
+
+    for i in range(h):
+        for j in range(w):
+
+            janela = l_pad[
+                i:i+tamanho_kernel,
+                j:j+tamanho_kernel
+            ]
+
+            l_filtrado[i, j] = np.min(janela)
+
+    img_lab_filtrada = cv2.merge([
+        l_filtrado,
+        a,
+        b
+    ])
+
+    img_lab_filtrada = np.clip(
+        img_lab_filtrada,
+        0,
+        255
+    ).astype(np.uint8)
+
+    img_bgr_filtrada = cv2.cvtColor(
+        img_lab_filtrada,
+        cv2.COLOR_LAB2BGR
+    )
+
+    img_rgb = cv2.cvtColor(
+        img_bgr_filtrada,
+        cv2.COLOR_BGR2RGB
+    )
+
+    return img_rgb
+
+def filtro_maximo(img_bgr, tamanho_kernel=3):
+
+    img_lab = cv2.cvtColor(
+        img_bgr,
+        cv2.COLOR_BGR2LAB
+    ).astype(np.float64)
+
+    l, a, b = cv2.split(img_lab)
+
+    h, w = l.shape
+
+    l_filtrado = np.zeros_like(l)
+
+    margem = tamanho_kernel // 2
+
+    l_pad = np.pad(
+        l,
+        ((margem, margem),
+         (margem, margem)),
+        mode='edge'
+    )
+
+    for i in range(h):
+        for j in range(w):
+
+            janela = l_pad[
+                i:i+tamanho_kernel,
+                j:j+tamanho_kernel
+            ]
+
+            l_filtrado[i, j] = np.max(janela)
+
+    img_lab_filtrada = cv2.merge([
+        l_filtrado,
+        a,
+        b
+    ])
+
+    img_lab_filtrada = np.clip(
+        img_lab_filtrada,
+        0,
+        255
+    ).astype(np.uint8)
+
+    img_bgr_filtrada = cv2.cvtColor(
+        img_lab_filtrada,
+        cv2.COLOR_LAB2BGR
+    )
+
+    img_rgb = cv2.cvtColor(
+        img_bgr_filtrada,
+        cv2.COLOR_BGR2RGB
+    )
+
+    return img_rgb
+
+def filtro_midpoint(img_bgr, tamanho_kernel=3):
+
+    img_lab = cv2.cvtColor(
+        img_bgr,
+        cv2.COLOR_BGR2LAB
+    ).astype(np.float64)
+
+    l, a, b = cv2.split(img_lab)
+
+    h, w = l.shape
+
+    l_filtrado = np.zeros_like(l)
+
+    margem = tamanho_kernel // 2
+
+    l_pad = np.pad(
+        l,
+        ((margem, margem),
+         (margem, margem)),
+        mode='edge'
+    )
+
+    for i in range(h):
+        for j in range(w):
+
+            janela = l_pad[
+                i:i+tamanho_kernel,
+                j:j+tamanho_kernel
+            ]
+
+            l_filtrado[i, j] = (1/2) * (np.max(janela) + np.min(janela))
+
+    img_lab_filtrada = cv2.merge([
+        l_filtrado,
+        a,
+        b
+    ])
+
+    img_lab_filtrada = np.clip(
+        img_lab_filtrada,
+        0,
+        255
+    ).astype(np.uint8)
+
+    img_bgr_filtrada = cv2.cvtColor(
+        img_lab_filtrada,
+        cv2.COLOR_LAB2BGR
+    )
+
+    img_rgb = cv2.cvtColor(
+        img_bgr_filtrada,
+        cv2.COLOR_BGR2RGB
+    )
+
+    return img_rgb
