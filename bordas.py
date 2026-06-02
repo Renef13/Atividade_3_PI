0787a724b538f2bbc1dc84d6e80ff81251fbed6b
Renef <renefricardo30@gmail.com>
2026-06-02 15:42:36 -0300

    add comparação

--- bordas.py
+++ bordas.py
@@ -20,3 +20,18 @@ def det_roberts(img):
     magnitude = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
 
     return magnitude.astype(np.uint8)
+
+
+def det_prewitt(img):
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(np.float32)
+    kernel_gx = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], dtype=np.float32)
+    kernel_gy = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]], dtype=np.float32)
+
+    gx = convolve(img_gray, kernel_gx, mode='reflect')
+    gy = convolve(img_gray, kernel_gy, mode='reflect')
+
+    magnitude = np.sqrt(gx ** 2 + gy ** 2)
+
+    magnitude = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+
+    return magnitude.astype(np.uint8)
