5c7fa1249145a8dc057d3db02bbab8485cd4313e
Renef <renefricardo30@gmail.com>
2026-06-08 15:06:30 -0300

    add funcao de det_scharr

--- bordas.py
+++ bordas.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2 as cv
 from scipy.ndimage import convolve
 
+from det_bordas import kernel_gx
+
 
 def det_roberts(img):
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(np.float64)
@@ -54,7 +56,21 @@ def det_sobel(img):
 
 
 def det_scharr(img):
-    pass
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(np.float64)
+
+    kernel_gx = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]], dtype=np.float64)
+    kernel_gy = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]], dtype=np.float64)
+
+    gx = convolve(img_gray, kernel_gx, mode='reflect')
+    gy = convolve(img_gray, kernel_gy, mode='reflect')
+
+    magnitude = np.sqrt(gx ** 2 + gy ** 2)
+
+    magnitude = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+
+    return magnitude.astype(np.uint8)
+
+
 
 
 def det_laplaciano(img):
